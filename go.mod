268208f09682689d1c71cd2c5ecc181a485bc6eb
Lukas SP <lukaesebrot@gmail.com>
2020-05-11 20:06:33 +0200

    Implement bot configuration initialization

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lukaesebrot/asterisk
 
 go 1.14
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"log"
+
+	"github.com/Lukaesebrot/asterisk/config"
 )
 
 func main() {
 	log.Println("Starting this Asterisk instance...")
 
-	// TODO: Implement configuration
+	// Initialize the configuration
+	log.Println("Loading the bot configuration...")
+	err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Successfully loaded the bot configuration.")
 
 	// TODO: Implement MongoDB integration
 
