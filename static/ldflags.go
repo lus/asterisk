6a988430bab2077cd42809050a1eae3e90719172
Lukas SP <lukaesebrot@gmail.com>
2020-05-11 20:05:48 +0200

    Implement ldflags

---
+++ static/ldflags.go
@@ -0,0 +1,9 @@
+package static
+
+var (
+	// Mode defines whether or not the bot runs in production mode
+	Mode = "dev"
+
+	// Version defines the version string of the current instance
+	Version = "dev"
+)
