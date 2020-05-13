bad2575b49a4c1dc7ad475b6da849a161f4fae89
Lukas SP <lukaesebrot@gmail.com>
2020-05-14 01:03:30 +0200

    Implement timeout for feature requests

--- commands/request.go
+++ commands/request.go
@@ -1,14 +1,27 @@
 package commands
 
 import (
+	"time"
+
 	"github.com/Lukaesebrot/asterisk/config"
 	"github.com/Lukaesebrot/asterisk/utils"
 	"github.com/Lukaesebrot/dgc"
 	"github.com/bwmarrin/discordgo"
 )
 
+// lastRequestCalls holds the timestamp at which the given user ID (key) last executed the request command
+var lastRequestCalls = make(map[string]int64)
+
 // Request handles the request command
 func Request(ctx *dgc.Ctx) {
+	// Check if the user is being time-outed
+	lastCall := lastRequestCalls[ctx.Event.Member.User.ID]
+	currentTime := time.Now().UnixNano() / 1e6
+	if (lastCall != 0) && (currentTime-lastCall) < 3600000 {
+		ctx.Session.ChannelMessageSendEmbed(ctx.Event.ChannelID, utils.GenerateErrorEmbed("You need to wait at least one hour between two feature requests."))
+		return
+	}
+
 	// Validate the input
 	if ctx.Arguments.Amount() == 0 {
 		ctx.Session.ChannelMessageSendEmbed(ctx.Event.ChannelID, utils.GenerateInvalidUsageEmbed("You need to specify a feature you want to request."))
@@ -22,6 +35,9 @@ func Request(ctx *dgc.Ctx) {
 		return
 	}
 	ctx.Session.MessageReactionAdd(config.CurrentConfig.FeatureRequestChannel, message.ID, "✅")
+
+	// Save the current execution time
+	lastRequestCalls[ctx.Event.Member.User.ID] = currentTime
 
 	// Confirm the creation of the feature request
 	ctx.Session.ChannelMessageSendEmbed(ctx.Event.ChannelID, utils.GenerateSuccessEmbed("Your feature request was submitted."))
