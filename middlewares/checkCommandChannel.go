2a7e19c537365d0ec3fca188e529b6976825d955
Lukas SP <lukaesebrot@gmail.com>
2020-05-14 02:07:15 +0200

    Allow guild admins to execute commands in non-command channels

--- middlewares/checkCommandChannel.go
+++ middlewares/checkCommandChannel.go
@@ -4,10 +4,15 @@ import (
 	"github.com/Lukaesebrot/asterisk/guildconfig"
 	"github.com/Lukaesebrot/asterisk/utils"
 	"github.com/Lukaesebrot/dgc"
+	"github.com/bwmarrin/discordgo"
 )
 
 // CheckCommandChannel checks if the current channel is a valid command channel
 func CheckCommandChannel(ctx *dgc.Ctx) bool {
 	guildConfig := ctx.CustomObjects["guildConfig"].(*guildconfig.GuildConfig)
-	return !guildConfig.ChannelRestriction || utils.StringArrayContains(guildConfig.CommandChannels, ctx.Event.ChannelID)
+	if guildConfig.ChannelRestriction && !utils.StringArrayContains(guildConfig.CommandChannels, ctx.Event.ChannelID) {
+		isAdmin, _ := utils.HasPermission(ctx.Session, ctx.Event.GuildID, ctx.Event.Author.ID, discordgo.PermissionAdministrator)
+		return isAdmin
+	}
+	return true
 }
