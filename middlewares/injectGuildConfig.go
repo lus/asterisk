27283a53c905c688724f0d9dd38018ef6f9dc350
Lukas SP <lukaesebrot@gmail.com>
2020-05-22 02:16:03 +0200

    Implement guild config cache

--- middlewares/injectGuildConfig.go
+++ middlewares/injectGuildConfig.go
@@ -9,7 +9,7 @@ import (
 // InjectGuildConfig injects the guild configuration into the custom context objects
 func InjectGuildConfig(ctx *dgc.Ctx) bool {
 	// Retrieve the guild configuration
-	guildConfig, err := guildconfig.Retrieve(ctx.Event.GuildID)
+	guildConfig, err := guildconfig.RetrieveCached(ctx.Event.GuildID)
 	if err != nil {
 		ctx.Session.ChannelMessageSendEmbed(ctx.Event.ChannelID, utils.GenerateErrorEmbed(err.Error()))
 		return false
