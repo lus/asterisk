fcd879adc203fb6c936752b9a9fde91a3b97fb55
Lukas SP <lukaesebrot@gmail.com>
2020-06-05 22:42:51 +0200

    Except staff members from blacklist

--- middlewares/checkBlacklist.go
+++ middlewares/checkBlacklist.go
@@ -11,6 +11,12 @@ func CheckBlacklist(next dgc.ExecutionHandler) dgc.ExecutionHandler {
 		// Retrieve the current user
 		user := ctx.CustomObjects.MustGet("user").(*users.User)
 
+		// Check if the user is a staff member
+		if user.HasFlag(users.FlagAdministrator) || user.HasFlag(users.FlagModerator) {
+			next(ctx)
+			return
+		}
+
 		// Check if the user has the 'blacklisted' flag
 		isBlacklisted := user.HasFlag(users.FlagBlacklisted)
 		if isBlacklisted {
