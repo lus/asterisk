d8735cd1c67c1aacf19c40fae27c1040318666bf
Zordem <38507337+Zordem@users.noreply.github.com>
2020-06-16 16:16:43 +0200

    discord.comm => discordapp.com
    Weil wegen angenehmer.

--- embeds/starboard.go
+++ embeds/starboard.go
@@ -10,7 +10,7 @@ import (
 // Starboard generates a starboard embed
 func Starboard(message *discordgo.Message) *discordgo.MessageEmbed {
 	return &discordgo.MessageEmbed{
-		URL:         fmt.Sprintf("https://discord.com/channels/%s/%s/%s/", message.GuildID, message.ChannelID, message.ID),
+		URL:         fmt.Sprintf("https://discordapp.com/channels/%s/%s/%s/", message.GuildID, message.ChannelID, message.ID),
 		Title:       "Jump To Message",
 		Description: message.Content,
 		Timestamp:   time.Now().Format(time.RFC3339),
