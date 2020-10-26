63f920ebcb37524e06ed209f007e27c6c5ccf546
zekroTJA <riho@live.de>
2020-10-26 21:34:39 +0100

    change discordapp.com to discord.com (whoops)

--- embeds/starboard.go
+++ embeds/starboard.go
@@ -10,7 +10,7 @@ import (
 // Starboard generates a starboard embed
 func Starboard(message *discordgo.Message) *discordgo.MessageEmbed {
 	desc := fmt.Sprintf(
-		"%s\n\n[*jump to message*](https://discordapp.com/channels/%s/%s/%s)",
+		"%s\n\n[*jump to message*](https://discord.com/channels/%s/%s/%s)",
 		message.Content, message.GuildID, message.ChannelID, message.ID)
 
 	emb := &discordgo.MessageEmbed{
