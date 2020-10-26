df52d3d9e9b9bc8c2affc9dcd5c28a20ac7c7d83
zekroTJA <riho@live.de>
2020-10-26 21:22:33 +0100

    fix 'jump to message' link

--- embeds/starboard.go
+++ embeds/starboard.go
@@ -9,10 +9,12 @@ import (
 
 // Starboard generates a starboard embed
 func Starboard(message *discordgo.Message) *discordgo.MessageEmbed {
+	desc := fmt.Sprintf(
+		"%s\n\n[*jump to message*](https://discordapp.com/channels/%s/%s/%s)",
+		message.Content, message.GuildID, message.ChannelID, message.ID)
+
 	return &discordgo.MessageEmbed{
-		URL:         fmt.Sprintf("https://discord.com/channels/%s/%s/%s/", message.GuildID, message.ChannelID, message.ID),
-		Title:       "Jump To Message",
-		Description: message.Content,
+		Description: desc,
 		Timestamp:   time.Now().Format(time.RFC3339),
 		Color:       0xffff00,
 		Author: &discordgo.MessageEmbedAuthor{
